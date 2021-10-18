02f5639cc0a354fc17223ba7a151c4c7db87bdbd
Tilier <tylerrosenblum2015@gmail.com>
2021-10-18 23:31:50 +0000

    nvm

--- app.js
+++ app.js
@@ -16,11 +16,11 @@ var con = mysql.createConnection({
 	database : '6OcHtB5ESO'
 });
 
-/* app.use(logger('dev'));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public'))); */
+app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', indexRouter);
 // app.use('/quotes', quotesRouter);
