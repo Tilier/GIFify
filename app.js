fa042aba1530feb4dad4f89b6b4a485bb4162cf1
Tilier <tylerrosenblum2015@gmail.com>
2021-10-18 18:06:27 +0000

    test

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ con.connect((err) => {
 });
 
 app.get('/', function (req, res) {
+  console.log(req.session.loggedin)
   if (req.session.loggedin == true) {
     res.sendFile(__dirname + '/public/index.html')
   } else {
