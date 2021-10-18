5c4ae2a38ff44a392c8a19b9dae13f2dbf76f588
Tilier <tylerrosenblum2015@gmail.com>
2021-10-18 17:37:57 +0000

    maybe this?

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ con.connect((err) => {
 });
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '../public', 'signup.html'))
+  res.sendFile(path.join(__dirname + 'public/signup.html')
 })
 
 app.post('/api/createaccount', function (req, res) {
