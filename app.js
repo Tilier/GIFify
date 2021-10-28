f76a9c6680f41f92dfcbf5fd9aaa56aa9afbfb0a
Tilier <tylerrosenblum2015@gmail.com>
2021-10-28 14:51:43 +0000

    testingggg

--- app.js
+++ app.js
@@ -117,7 +117,7 @@ app.post('/api/auth', function (req, res, next) {
           res.redirect('/')
           next();
         } else {
-          res.send(`incorrect password. result: ${JSON.stringify(result)} ~ result2: ${result2}`)
+          res.send(`incorrect password. result: ${result.password} ~ result2: ${result2}`)
         }
       })
     } else {
