e556c94bc2cd99a568bd79a4e01954ab763172de
Tilier <tylerrosenblum2015@gmail.com>
2021-11-03 17:15:34 +0000

    add sendGifMessgage() function

--- public/index.js
+++ public/index.js
@@ -1,1 +1,12 @@
-console.log('Hello world from client-side JS!')
+function sendGifMessage() {
+	$.ajax({ 
+		url: "/api/send-gif-message", 
+		data: {...}, 
+		type: "GET", // if you want to send data via the "data" property change this to "POST". This can be omitted otherwise 
+		success: function(responseData) { 
+			// empty mcmuffin
+      	}, 
+    	error: console.error 
+    }); 
+
+}
