d963b4336b5b364d5ff6a7e5ca0577fc8823f66c
Tilier <tylerrosenblum2015@gmail.com>
2021-11-03 17:27:47 +0000

    edit creategifmessage link

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 function sendGifMessage() {
 	$.ajax({ 
-		url: "/api/send-gif-message", 
+		url: "/api/sendgifmessage", 
 		data: {...}, 
 		type: "GET", // if you want to send data via the "data" property change this to "POST". This can be omitted otherwise 
 		success: function(responseData) { 
