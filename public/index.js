458ba96ae19266147dc0fcbec2a1b554d03b3d2b
Tilier <tylerrosenblum2015@gmail.com>
2021-11-03 22:15:40 +0000

    add data to ajax request

--- public/index.js
+++ public/index.js
@@ -1,8 +1,13 @@
 function sendGifMessage() {
 	$.ajax({ 
-		url: "/api/sendgifmessage", 
-		data: {...}, 
-		type: "GET", // if you want to send data via the "data" property change this to "POST". This can be omitted otherwise 
+		url: "/api/sendgifmessage",
+		data: {
+			sender: "Tilier",
+			receiver: "banana",
+			gif: "idk",
+			caption: "idkkkkkk"
+		}, 
+		type: "POST", // if you want to send data via the "data" property change this to "POST". This can be omitted otherwise 
 		success: function(responseData) { 
 			// empty mcmuffin
       	}, 
