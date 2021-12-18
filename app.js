d8083762c3bb5d56d417a2183fa717f94c6dc489
Tilier <61211693+Tilier@users.noreply.github.com>
2021-12-18 10:28:29 -0500

    remove handleDisconnect() calls

--- app.js
+++ app.js
@@ -26,14 +26,14 @@ connection = mysql.createConnection(db_config);
 connection.connect(function(err) {
   if(err) {
     console.log('error when connecting to db:', err);
-    setTimeout(handleDisconnect, 2000)
+    // setTimeout(handleDisconnect, 2000)
   }
 });
 
 connection.on('error', function(err) {
   console.log('db error', err);
   if(err.code === 'PROTOCOL_CONNECTION_LOST') {
-    handleDisconnect();                         // lost due to either server restart, or a
+    // handleDisconnect();                         // lost due to either server restart, or a
   } else {                                      // connnection idle timeout (the wait_timeout
     throw err;                                  // server variable configures this)
   }
